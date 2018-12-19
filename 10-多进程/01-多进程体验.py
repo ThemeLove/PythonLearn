529b2cc26eb0af31fb5d6f75a3acc9e7eb694009
themelove <lqsthemelove@163.com>
2018-12-19 14:41:29 +0800

    modify

--- 10-多进程/01-多进程体验.py
+++ 10-多进程/01-多进程体验.py
@@ -3,17 +3,17 @@ import threading
 import time
 
 
-def test1():
+def test1(sleep_time):
     while True:
-        time.sleep(1)
+        time.sleep(sleep_time)
         print("-----test1-----processName= "+multiprocessing.current_process().name+" ;threadName= "+threading.current_thread().getName())
         # -----test1-----processName= Process-1 ;threadName= MainThread
         # 每个进程里默认都有个主线程
 
 
-def test2():
+def test2(sleep_time):
     while True:
-        time.sleep(1)
+        time.sleep(sleep_time)
         print("-----test2-----processName=" + multiprocessing.current_process().name+" ;threadName= "+threading.current_thread().getName())
         # -----test2-----processName=Process-2 ;threadName= MainThread
         # 每个进程里默认都有个主线程
@@ -25,13 +25,18 @@ def main():
     print("cpu_count="+str(cpu_count))
 
     # 创建多进程
-    process1 = multiprocessing.Process(target=test1)
-    process2 = multiprocessing.Process(target=test2)
+    process1 = multiprocessing.Process(target=test1, args=(1,))
+    process2 = multiprocessing.Process(target=test2, args=(1,))
     # 开启多进程
     process1.start()
     process2.start()
     print("-----main-----processName=" + multiprocessing.current_process().name + " ;threadName= " + threading.current_thread().getName())
     # -----main-----processName=MainProcess ;threadName= MainThread
+
+    process1.join(5) # process1阻塞主进程5秒
+    print("5 second later")
+    process1.join(5) # process2阻塞主进程5秒
+    print("5 second later") # process1 和 process2 一共阻塞主进程10秒，10秒后才能打印该行
 
 
 if __name__ == "__main__":
