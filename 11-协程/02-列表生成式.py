5bb8c7220c1e5bdec40e696ff87902380c9a38c7
themelove <lqsthemelove@163.com>
2018-12-24 11:49:37 +0800

    modify

--- 11-协程/02-列表生成式.py
+++ 11-协程/02-列表生成式.py
@@ -1,19 +1,19 @@
-
-
 def main():
 
     """
         这里的list生成方式就是列表生成式（list comprehensions）,可以大大提高对list的操作效率
+        语法： [表达式]
+        return :list
     """
-    lista = [x**2 for x in range(1, 11) if x % 2 == 0]
-    print("lista= ", lista)
-    for temp in lista:
+    list1 = [x**2 for x in range(1, 11) if x % 2 == 0]
+    print("list1= ", list1)
+    for temp in list1:
         print(temp)
 
-    listb = [m+n for m in "abc" for n in "123"]
-    print("listb= ",listb)
+    list2 = [m+n for m in "abc" for n in "123"]
+    print("list2= ", list2)
 
-    print(lista)
+    print(list2)
 
 
 if __name__ == "__main__":
