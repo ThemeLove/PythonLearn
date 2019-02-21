71f025a7074589cb0e2f11059b25db2b2ad45a6f
themelove <lqsthemelove@163.com>
2019-02-21 18:04:53 +0800

    modify

--- Python基础/测试/05-json模块的用法.py
+++ Python基础/测试/05-json模块的用法.py
@@ -23,6 +23,26 @@ def main():
         dict_c = json.load(f)
         print("type of dict_c=" + str(type(dict_c)))
 
+    stu1 = Student("zhangsan", 23)
+
+    # 将自定义对象转化为json字符串，lambda obj:obj.__dict__ 将任意obj转化为dict,json.dumps再将dict转化为json字符串
+    stu_str = json.dumps(stu1, default=lambda obj: obj.__dict__)
+    print("stu_str="+stu_str)
+
+    # 将json字符串，转化为自定义对象,需要传入关键字参数object_hook指明转化方式
+    stu2 = json.loads(stu_str, object_hook=dict2student)
+    print("type of stu2=" + str(type(stu2)))
+
+
+def dict2student(d):
+    return Student(d["name"], d["age"])
+
+
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
 
 if __name__ == "__main__":
     main()
